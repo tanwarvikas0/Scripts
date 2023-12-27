bb530cdfaedbc5863948409d3a8c78f3711d06b5
tanwarvikas0 <tanwarv224@gmail.com>
2023-12-27 13:28:25 +0530

    modified last

--- uptime.sh
+++ uptime.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 > uptime_info.txt
-user_name='ctr_support'
-password='ctr_support321'
-read -p "Please Enter the file name:" List
+user_name=' '   ## enter your username
+password=' '    ## enter your password 
+read -p "Please Enter the file name:" List   ## in the list give the list of ip or server file
 diskspace() 
 {
         sshpass -p $password ssh -o stricthostkeychecking=no $user_name@$ip<< EOF
@@ -12,8 +12,10 @@ EOF
 }
 cat $List | while read ip
 do
-	echo "Uptime information for "$ip >> uptime_info.txt
-      # diskspace | sed s/%//g | awk '{ if($5 > 80) print $0;}' >> uptime_info.txt
+	echo "Uptime information for "$ip >> uptime_info.txt      # use for check all server uptime
+	
+      # diskspace | sed s/%//g | awk '{ if($5 > 80) print $0;}' >> uptime_info.txt  
+	
         diskspace >> uptime_info.txt
 	if [ $? -gt 0 ]; then
 		echo "Not able to ssh "$ip >> uptime_info.txt
