4fdb781dc88b62dd261d0fd0e4f57f1995f0a49c
tanwarvikas0 <tanwarv224@gmail.com>
2023-12-27 13:49:37 +0530

    check the replication status of mysql and how behind the server is

---
+++ replication_trigger.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+ip=`hostname -I|awk '{print $1}'`
+binfile="mysql"
+uname="BACKUSER"
+pwd="support@"
+cmd=`$binfile -u$uname -p$pwd  -e "show slave status\G;"`
+if [[ $? -gt 0 ]]; then
+        echo "Not able to connect with MYSQL slave node "$ip |mail -s "Ithuba | Mysql Connection Failed on $ip for Replication Check" dba@skilrock.com monitoring@skilrock.com
+exit
+fi
+sbh=`echo "$cmd" |  grep Seconds_Behind_Master|head -n 1|awk '{print $2}'`
+echo "current="$sbh >> /home/ctr_support/replicationCheck/data.txt
+prev=`grep prev /home/ctr_support/replicationCheck/data.txt | gawk -F'=' '{print $2}'`
+current=`grep current /home/ctr_support/replicationCheck/data.txt | gawk -F'=' '{print $2}'`
+if [[ $? -eq 0 ]]; then
+        > /home/ctr_support/replicationCheck/repl_monitor.txt
+        echo '' >> /home/ctr_support/replicationCheck/repl_monitor.txt
+        echo "Replication Detail of SlaveNode $ip" >> /home/ctr_support/replicationCheck/repl_monitor.txt
+        echo '' >> /home/ctr_support/replicationCheck/repl_monitor.txt
+        echo `echo "$cmd" |  grep Master_Host` >> /home/ctr_support/replicationCheck/repl_monitor.txt
+        echo `echo "$cmd" |  grep Master_Log_File | head -n 1` >> /home/ctr_support/replicationCheck/repl_monitor.txt
+        echo `echo "$cmd" |  grep Slave_IO_Running` >> /home/ctr_support/replicationCheck/repl_monitor.txt
+        echo `echo "$cmd" |  grep Slave_SQL_Running|head -n 1` >> /home/ctr_support/replicationCheck/repl_monitor.txt
+        echo `echo "$cmd" |  grep Read_Master_Log_Pos` >> /home/ctr_support/replicationCheck/repl_monitor.txt
+        echo `echo "$cmd" |  grep Exec_Master_Log_Pos` >> /home/ctr_support/replicationCheck/repl_monitor.txt
+        echo `echo "$cmd" |  grep Seconds_Behind_Master` >> /home/ctr_support/replicationCheck/repl_monitor.txt
+        echo `echo "$cmd" |  grep Last_IO_Error` >> /home/ctr_support/replicationCheck/repl_monitor.txt
+        echo `echo "$cmd" |  grep Last_SQL_Error` >> /home/ctr_support/replicationCheck/repl_monitor.txt
+        var=`cat /home/ctr_support/replicationCheck/repl_monitor.txt`
+        slaveio=`echo "$cmd"|grep Slave_IO_Running|head -n 1|awk '{print $2}'`
+        slavesql=`echo "$cmd"|grep Slave_SQL_Running|head -n 1|awk '{print $2}'`
+        if [[ $slaveio == 'No' || $slavesql == 'No' ]]; then
+                echo "$var"|mail -s "Mysql Replication for $ip is reporting Error" dba@skilrock.com monitoring@skilrock.com
+        #sbh=`echo "$cmd" |  grep Seconds_Behind_Master|head -n 1|awk '{print $2}'
+        elif [[ $sbh -gt 600 || $sbh == 'NULL' ]]; then
+                echo "prev="$sbh > /home/ctr_support/replicationCheck/data.txt
+                echo "$var"|mail -s "Mysql Replication Reporting Delay for $ip" dba@skilrock.com monitoring@skilrock.com
+        elif [[ $prev -gt '200'  &&  $current == '0' ]]; then
+                echo "$var"|mail -s "Resolved : Ithuba | Mysql Replication Reporting Delay for $ip" dba@skilrock.com monitoring@skilrock.com
+        else
+                >/home/ctr_support/replicationCheck/data.txt
+                echo "prev="$current > /home/ctr_support/replicationCheck/data.txt
+                exit
+        fi
+fi
